eac32107c52f6b6fcc322392b8eb735295c368d5
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-14 17:49:08 +0200

    fix: move movies in a dedicated file (db.php)

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+// per creare istanze di Movie
+$ilSignoreDegliAnelli = new Movie("Il signore degli anelli", "Peter Jackson", 2001, [$fantasy, $azione, $drammatico], 180);
+$avengers = new Movie("Avengers", "Josh Whedon", 2012, [$azione], 10);
+$bohemianRhapsody = new Movie("Bohemian Rhapsody", "Bryan Singer", 2018, [$biografico, $musical], 134);
+$matrix = new Movie("Matrix", "Lana e Lilly Wachowski", 1999, [$fantascienza, $azione, $thriller], 136);
+$titanic = new Movie("Titanic", "James Cameron", 1997, [$storico, $romantico, $drammatico], 195);
+$coco = new Movie("Coco", "Lee Unkrich", 2017, [$animazione, $musical, $fantasy], 105);
+
+?>
